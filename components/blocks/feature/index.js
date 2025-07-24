c2ccc4219616a3d6827a2009108aaffdcd3cdcf2
Paras Bokhari <parasbokhari@gmail.com>
2025-07-24 17:16:02 -0500

    removing featurevariant04

--- components/blocks/feature/index.js
+++ components/blocks/feature/index.js
@@ -1,11 +1,5 @@
 import FeatureVariant01 from "./FeatureVariant01";
 import FeatureVariant02 from "./FeatureVariant02";
 import FeatureVariant03 from "./FeatureVariant03";
-import FeatureVariant04 from "./FeatureVariant04";
 
-export {
-  FeatureVariant01,
-  FeatureVariant02,
-  FeatureVariant03,
-  FeatureVariant04,
-};
+export { FeatureVariant01, FeatureVariant02, FeatureVariant03 };
